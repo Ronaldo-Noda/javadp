cf37449f0310914550e08d077e223e13bee21d6b
Ronaldo Noda <156722634+Ronaldo-Noda@users.noreply.github.com>
2024-12-03 20:40:05 -0300

    Add files via upload

---
+++ javadp/live-next2024-main/src/main/java/br/com/fiap/Main.java
@@ -0,0 +1,52 @@
+package br.com.fiap;
+
+import br.com.fiap.games.filter.CorsFilter;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jsonb.JsonBindingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in br.com.fiap package
+        final ResourceConfig rc = new ResourceConfig().packages("br.com.fiap");
+        rc.register(JsonBindingFeature.class);
+        rc.register(CorsFilter.class);
+
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080"; // Porta padrão para desenvolvimento local
+        }
+        URI baseUri = URI.create("http://0.0.0.0:" + port + "/");
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(baseUri, rc);
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with endpoints available at "
+                + "%s%nHit Ctrl-C to stop it...", BASE_URI));
+    }
+}
+
